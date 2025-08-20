ffcb324eca83a09d2c2d3b01e0d4b2eba358b4b3
23eg107b21-source <23eg107b21@anurag.edu.in>
2025-08-20 13:47:53 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'college lost and found',message:'The College Lost and Found Portal is a digital platform where students and staff can easily report lost items and claim found belongings within the campus.' });
+  res.render('index', { title: 'College Lost and Found',message:'The College Lost and Found Portal is a digital platform where students and staff can easily report lost items and claim found belongings within the campus.' });
 });
 
 module.exports = router;
